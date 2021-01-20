19301e0b1c86d6004a1bd30a1c56c2a8506f0316
polariche <polariche@gmail.com>
2021-01-20 08:20:27 +0000

    remove transform reference

--- source/loaders.py
+++ source/loaders.py
@@ -4,7 +4,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.utils.data import  Dataset, DataLoader
-from torchvision import transform
 import re
 import warnings
 
