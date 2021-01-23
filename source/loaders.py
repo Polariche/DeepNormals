81a9786b3551e07a1b5928ee808c9e6b02fa4c4d
polariche <polariche@gmail.com>
2021-01-23 11:07:30 +0000

    normals should be displayed correctly

--- source/loaders.py
+++ source/loaders.py
@@ -49,7 +49,7 @@ class ObjDataset(Dataset):
         vn = torch.zeros_like(v)
 
         # add face normal to connected vertices
-        vn.index_add(0, f.view(-1), fn.unsqueeze(1).repeat(1,3).view(-1))
+        vn.index_add(0, f.view(-1), fn.unsqueeze(0).repeat(1,3,1).view(-1,3))
 
         # normalization
         vn = vn / torch.norm(vn, dim=1, keepdim=True)
