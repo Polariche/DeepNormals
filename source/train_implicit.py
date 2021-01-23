d0f28aa2efa5be764050d73f6b5ff1e3960314bb
polariche <polariche@gmail.com>
2021-01-23 09:04:22 +0000

    didnt compute normals lol

--- source/train_implicit.py
+++ source/train_implicit.py
@@ -123,8 +123,8 @@ def main():
         writer.add_scalars("loss", {'train': loss_t}, epoch)
 
         if epoch % 10 == 0:
-            writer.add_mesh("s", xyz_aug.unsqueeze(0), colors=(s.unsqueeze(0).repeat(1,1,3) * 128 + 128).int(), global_step=epoch)
-            writer.add_mesh("n", xyz_aug.unsqueeze(0), colors=(n.unsqueeze(0) * 128 + 128).int(), global_step=epoch)
+            writer.add_mesh("s", xyz_aug[:xyz.shape[0]].unsqueeze(0), colors=(s[:xyz.shape[0]].unsqueeze(0).repeat(1,1,3) * 128 + 128).int(), global_step=epoch)
+            writer.add_mesh("n", xyz_aug[:xyz.shape[0]].unsqueeze(0), colors=(n[:xyz.shape[0]].unsqueeze(0) * 128 + 128).int(), global_step=epoch)
 
         # update
         optimizer.step()
