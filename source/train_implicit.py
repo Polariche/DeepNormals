8c5932510c1387d522abd189969336656a17db74
polariche <polariche@gmail.com>
2021-01-25 16:45:40 +0000

    input can be gradient-descented

--- source/train_implicit.py
+++ source/train_implicit.py
@@ -101,7 +101,7 @@ def main():
 
 
     # load 
-    ds = ObjDataset("../../../data/train/02828884/model_042328.obj")
+    ds = ObjDataset("../../../data/train/02828884/model_005004.obj")
 
     n = ds.vn
     xyz = ds.v
