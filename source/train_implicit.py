70be9d9dc8cdeb159c32ae9700552aa3a88e7055
polariche <polariche@gmail.com>
2021-01-25 17:15:56 +0000

    input can be gradient-descented

--- source/train_implicit.py
+++ source/train_implicit.py
@@ -64,7 +64,7 @@ def train(device, model, xyz, s_gt, n_gt, backward=True, lamb=0.005):
         p_gt = torch.exp(-s_gt / (2*1e-4))
 
     # instead of using n_gt, we could use ECPN tangent loss
-    loss_grad = 1e2 * torch.mean((1 - torch.sum(n * n_gt, dim=1, keepdim=True)) * p_gt)
+    loss_grad = 1e2 * torch.mean((1 - torch.sum(n * n_gt, dim=1, keepdim=True) / torch.norm(n, dim=1, keepdim=True)) * p_gt)
     
     loss_zeros = 3e3 * torch.mean(torch.abs(s) * p_gt)
     loss_ones = 1e2 * torch.mean(torch.exp(-1e2*s) * (1-p_gt))
