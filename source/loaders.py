13e142b69401a6af7ff6a3a1813f219ec5d5c8a8
polariche <polariche@gmail.com>
2021-01-25 09:09:43 +0000

    proper normals

--- source/loaders.py
+++ source/loaders.py
@@ -35,7 +35,7 @@ class ObjDataset(Dataset):
 
         # obtain face normal with cross vector
         a1 = - vf[:,0] + vf[:,1] 
-        a2 = - vf[:,0] + vf[:,2]
+        a2 = - vf[:,2] + vf[:,1]
         
         fn = torch.cat([t.unsqueeze(1) for t in 
            [a1[:,1] * a2[:,2] - a1[:,2] * a2[:,1],
