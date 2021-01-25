f9817e075b0ebc8c6531411ec99518ab8c3b7486
polariche <polariche@gmail.com>
2021-01-25 14:53:15 +0000

    input can be gradient-descented

--- source/train_implicit.py
+++ source/train_implicit.py
@@ -134,9 +134,12 @@ def main():
 
         # visualization
         n_normalized = n / torch.norm(n, dim=1, keepdim=True)
-        n_error = torch.acos(torch.sum(n_normalized * n_aug, dim=1, keepdim=True)) / torch.acos(1)
+        n_error = torch.sum(n_normalized * n_aug, dim=1, keepdim=True) / torch.norm(n_aug, dim=1, keepdim=True)
+        n_error = torch.acos(n_error) / np.arccos(0)
 
-        writer.add_scalars("normal error", {'train': n_error[:xyz.shape[0]].detach().mean()}, epoch)
+        n_error_originals = n_error[:xyz.shape[0]]
+
+        writer.add_scalars("normal error", {'train': n_error_originals[~torch.isnan(n_error_originals)].detach().mean()}, epoch)
 
         if epoch % 10 == 0:
             writer.add_mesh("2. n", xyz_aug[xyz.shape[0]:].unsqueeze(0), colors=(n_normalized[:xyz.shape[0]:].unsqueeze(0) * 128 + 128).int(), global_step=epoch)
