22a51bd7eb895996024a8727b11f1cb7f5e532e6
polariche <polariche@gmail.com>
2021-01-25 15:36:48 +0000

    input can be gradient-descented

--- source/train_implicit.py
+++ source/train_implicit.py
@@ -118,7 +118,7 @@ def main():
     writer.add_mesh("1. n_gt", xyz.unsqueeze(0), colors=(n.unsqueeze(0) * 128 + 128).int(), faces=ds.f.unsqueeze(0))
     
 
-    optimizer = optim.Adam(list(model.parameters()) + [xyz_aug], lr = 1e-4)
+    optimizer = optim.Adam(list(model.parameters()) + [xyz_aug[xyz.shape[0]:]], lr = 1e-4)
 
     for epoch in range(args.epoch):
         loss_t = 0
@@ -147,8 +147,8 @@ def main():
             writer.add_scalars("normal error", {'train': n_error_originals[~torch.isnan(n_error_originals)].detach().mean()}, epoch)
             
             if epoch % 10 == 0:
-                writer.add_mesh("2. n", xyz_aug[:xyz.shape[0]].unsqueeze(0), colors=(n_normalized[:xyz.shape[0]].unsqueeze(0) * 128 + 128).int(), global_step=epoch, faces=ds.f.unsqueeze(0))
-                writer.add_mesh("3. n_error", xyz_aug[:xyz.shape[0]].unsqueeze(0), colors=(F.pad(n_error[:xyz.shape[0]], (0,2)).unsqueeze(0) * 256).int(), global_step=epoch, faces=ds.f.unsqueeze(0))
+                writer.add_mesh("2. n", xyz_aug[xyz.shape[0]:].unsqueeze(0), colors=(n_normalized[xyz.shape[0]:].unsqueeze(0) * 128 + 128).int(), global_step=epoch, faces=ds.f.unsqueeze(0))
+                writer.add_mesh("3. n_error", xyz_aug[xyz.shape[0]:].unsqueeze(0), colors=(F.pad(n_error[xyz.shape[0]:], (0,2)).unsqueeze(0) * 256).int(), global_step=epoch, faces=ds.f.unsqueeze(0))
 
         # update
         optimizer.step()
