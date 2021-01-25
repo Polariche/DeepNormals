391150d6ea0500beb6aa9e74527c2b95cf030f77
polariche <polariche@gmail.com>
2021-01-25 16:35:00 +0000

    input can be gradient-descented

--- source/train_implicit.py
+++ source/train_implicit.py
@@ -92,7 +92,7 @@ def main():
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
     # create models
-    model = Siren(in_features=3, out_features=1, hidden_features=256, hidden_layers=5, outermost_linear=True).to(device) 
+    model = Siren(in_features=3, out_features=1, hidden_features=256, hidden_layers=4, outermost_linear=True).to(device) 
 
     if args.weight != None:
         try:
