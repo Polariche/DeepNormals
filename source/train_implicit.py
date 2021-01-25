061f9213a2aca4174d85f3486819484ef31ace21
polariche <polariche@gmail.com>
2021-01-25 13:27:16 +0000

    tweak loss

--- source/train_implicit.py
+++ source/train_implicit.py
@@ -104,7 +104,7 @@ def main():
 
     with torch.no_grad():
         s_aug = torch.cat([torch.zeros((xyz.shape[0], 1)), torch.rand((xyz.shape[0], 1))], dim=0)
-        xyz_aug = torch.cat([xyz, xyz + n * s_aug[xyz.shape[0]:] * 0.01], dim=0)
+        xyz_aug = torch.cat([xyz, xyz + n * s_aug[xyz.shape[0]:] * 0.1], dim=0)
         n_aug = n.repeat(2,1)
 
         n_aug = n_aug.to(device)
