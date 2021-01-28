242a1072686857d17b4a938889c923a40a56cb60
polariche <polariche@gmail.com>
2021-01-28 08:03:59 +0000

    add random point regression

--- source/train_implicit.py
+++ source/train_implicit.py
@@ -61,7 +61,7 @@ def train(device, model, xyz, s_gt, n_gt, backward=True, lamb=0.005):
         p_gt = torch.exp(-s_gt / (2*1e-4))
 
     # instead of using n_gt, we could use ECPN tangent loss
-    loss_grad = 1e2 * torch.mean((1 - torch.sum(n * n_gt, dim=1, keepdim=True) / torch.norm(n, dim=1, keepdim=True)) * p_gt)
+    loss_grad = 1e2 * torch.mean((1 - torch.abs(torch.sum(n * n_gt, dim=1, keepdim=True)) / torch.norm(n, dim=1, keepdim=True)) * p_gt)
     
     loss_zeros = 3e3 * torch.mean(torch.abs(s) * p_gt)
     loss_ones = 1e2 * torch.mean(torch.exp(-1e2*s) * (1-p_gt))
@@ -159,7 +159,7 @@ def main():
 
             n_normalized = n / torch.norm(n, dim=1, keepdim=True)
             
-            n_error = torch.sum(n_normalized * n_aug, dim=1, keepdim=True) / torch.norm(n_aug, dim=1, keepdim=True)
+            n_error = torch.abs(torch.sum(n_normalized * n_aug, dim=1, keepdim=True)) / torch.norm(n_aug, dim=1, keepdim=True)
             n_error = torch.acos(n_error) / np.arccos(0)
 
             n_error_originals = n_error[:xyz.shape[0]]
