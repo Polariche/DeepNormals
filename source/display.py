063431255d39dd7e76c3e057f652a75bdf3644fe
polariche <polariche@gmail.com>
2021-01-28 05:46:11 +0000

    add display

---
+++ source/display.py
@@ -0,0 +1,95 @@
+import numpy as np
+import cv2
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from torch.utils.tensorboard import SummaryWriter
+
+from models import Siren
+from utils import Sobel
+from loaders import ObjDataset
+import utils
+from torch.utils.data import  DataLoader
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Test',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+parser.add_argument('data', metavar='DATA', help='path to file')
+
+parser.add_argument('--tb-save-path', dest='tb_save_path', metavar='PATH', default='../checkpoints/', 
+                        help='tensorboard checkpoints path')
+
+parser.add_argument('--weight-save-path', dest='weight_save_path', metavar='PATH', default='../weights/', 
+                        help='weight checkpoints path')
+
+parser.add_argument('--pretrained-weight', dest='weight', metavar='PATH', default=None, 
+                        help='pretrained weight')
+
+parser.add_argument('--activation', dest='activation', metavar='activation', default='relu', 
+                        help='activation of network; \'relu\' or \'sin\'')
+
+parser.add_argument('--batchsize', dest='batchsize', type=int, metavar='BATCHSIZE', default=1,
+                        help='batch size')
+parser.add_argument('--epoch', dest='epoch', type=int,metavar='EPOCH', default=100, 
+                        help='epochs')
+
+parser.add_argument('--epsilon', dest='epsilon', type=float, metavar='EPSILON', default=0.1, 
+                        help='epsilon')
+parser.add_argument('--omega', dest='omega', type=float, metavar='OMEGA', default=30, 
+                        help='hyperparameter for periodic layer')
+parser.add_argument('--lambda', dest='lamb', type=float, metavar='LAMBDA', default=0.005, 
+                        help='hyperparameter for s : normal loss ratio')
+
+
+parser.add_argument('--outfile', dest='outfile', metavar='OUTFILE', 
+                        help='output file')
+
+
+def main():
+    args = parser.parse_args()
+
+    writer = SummaryWriter(args.tb_save_path)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # create models
+    model = Siren(in_features=3, out_features=1, hidden_features=256, hidden_layers=5, outermost_linear=True).to(device) 
+
+    if args.weight != None:
+        try:
+            model.load_state_dict(torch.load(args.pretrained_weight))
+        except:
+            print("Couldn't load pretrained weight: " + args.pretrained_weight)
+
+    ds = ObjDataset("../../../data/train/02828884/model_049364.obj")
+
+    n = 256
+    with torch.no_grad():
+        voxel_min = torch.min(ds.v, dim=0, keepdim=True).to(device)
+        voxel_max = torch.max(ds.v, dim=0, keepdim=True).to(device)
+
+        voxels = torch.tensor([[i//n,i%n,0] for i in range(n*n)], dtype=torch.float) / n).to(device)
+        
+        ran = voxel_max - voxel_min
+
+        voxels *= ran
+
+    for i in range(256):
+        z = i/256 * ran[:,2]
+
+        with torch.no_grad():
+            voxels_ = voxels.clone().detach()
+            voxels_[:,2] = z
+
+        s, _ = model(voxels_)
+
+        writer.add_image("implicit", s.reshape(n,n), i, dataformats='WH')
+    
+    writer.close()
+
+
+if __name__ == "__main__":
+    main()
