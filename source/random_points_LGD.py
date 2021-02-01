9a1d72fc905d2e483226b46d9dd12d61a61adfb3
polariche <polariche@gmail.com>
2021-02-01 12:09:03 +0000

    random points

--- source/random_points_LGD.py
+++ source/random_points_LGD.py
@@ -87,10 +87,10 @@ def main():
 
         x_original = x.clone().detach()
 
-    layers_gen = lambda in_features, out_features: nn.Sequential(nn.Linear(in_features,32),*([nn.Linear(32,32)]*3),nn.Linear(32,out_features))
+    layers_gen = lambda in_features, out_features: nn.Sequential(nn.Linear(in_features,32,bias=False),*([nn.Linear(32,32,bias=False)]*5),nn.Linear(32,out_features,bias=False))
     
     lgd = LGD([x], layers_generator=layers_gen, n=50000).to(device)
-    optimizer = optim.Adam(lgd.parameters(), lr = 1e-2)
+    optimizer = optim.Adam(lgd.parameters(), lr = 5e-3)
 
     for i in range(500):
         lgd.zero_grad()
