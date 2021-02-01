034997b52c6cbacaf41bc98710e7cef21259f5f3
polariche <polariche@gmail.com>
2021-02-01 11:04:36 +0000

    random points

--- source/random_points_LGD.py
+++ source/random_points_LGD.py
@@ -115,7 +115,7 @@ def main():
             d2, _ = np.power(tree_new.query(xyz.cpu().numpy(), k=1),2)
 
             cd = (np.sum(d1) + np.sum(d2)) / 50000
-            cols = torch.clip((F.pad(torch.tensor(d1), (0,2)).unsqueeze(0) / 0.0001 * 256).int(), 0, 256)
+            cols = torch.clamp((F.pad(torch.tensor(d1), (0,2)).unsqueeze(0) / 0.0001 * 256).int(), 0, 256)
 
             writer.add_mesh("point cloud regressio_LGD", x.unsqueeze(0), colors=cols, global_step=i)
             writer.add_scalar("chamfer distance_LGD", cd, global_step=i)
