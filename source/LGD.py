6375dd120d9bb3aa1808f79a33afefdfd371cfda
polariche <polariche@gmail.com>
2021-02-23 11:23:10 +0000

    LGD v2

--- source/LGD.py
+++ source/LGD.py
@@ -108,7 +108,7 @@ class LGD(nn.Module):
             loss = loss_func(targets)
 
             loss_trajectory = loss
-            loss_trajectory += 1e-2*torch.pow(dx, 2).sum(dim=1).mean()     # regularizer for dx
+            #loss_trajectory += 1e-2*torch.pow(dx, 2).sum(dim=1).mean()     # regularizer for dx
 
             loss_trajectory.backward(retain_graph=True)
 
