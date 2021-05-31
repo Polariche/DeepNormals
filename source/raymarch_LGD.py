6902ba8d26978cad840e2adc14a4a67f7cb58ebd
polariche <polariche@gmail.com>
2021-05-31 14:45:25 +0000

    debugging intensifies

--- source/raymarch_LGD.py
+++ source/raymarch_LGD.py
@@ -77,14 +77,13 @@ def main():
         param.requires_grad = False
 
     
-    
     # load 
     mm = torch.tensor([1., 1., 1.], device=device, dtype=torch.float)
     mx = torch.tensor([-1., -1., 1.], device=device, dtype=torch.float)
     wh = torch.tensor([width, height, 1], device=device, dtype=torch.int)
 
-    rot = torch.tensor([[1,0,0], [0,1,0], [0,0,1]], device=device, dtype=torch.float)
-    trans = torch.tensor([[0,0,0]], device=device, dtype=torch.float)
+    rot = torch.tensor([[0.05,0,0], [0,0.05,0], [0,0,0.05]], device=device, dtype=torch.float)
+    trans = torch.tensor([[0, 0, -0.5]], device=device, dtype=torch.float)
 
     p_distribution = GridDataset(mm, mx, wh)
 
