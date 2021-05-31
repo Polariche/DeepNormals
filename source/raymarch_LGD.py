233a8b89a519adc532a52bd1f42d0bd106f24d31
polariche <polariche@gmail.com>
2021-05-31 13:57:36 +0000

    debugging intensifies

--- source/raymarch_LGD.py
+++ source/raymarch_LGD.py
@@ -163,9 +163,9 @@ def main():
 
         if i%10 == 0:
             #writer.add_scalars("regression_loss", {"LGD": loss}, global_step=i)
-            writer.add_mesh("point cloud regression_LGD", x.unsqueeze(0), global_step=i)
+            writer.add_mesh("point cloud regression_LGD", (d * n + trans).unsqueeze(0), global_step=i)
 
-            writer.add_scalars("chamfer_distance", {"LGD": chamfer_distance(x, p)}, global_step=i)
+            #writer.add_scalars("chamfer_distance", {"LGD": chamfer_distance(x, p)}, global_step=i)
         
     writer.close()
 
