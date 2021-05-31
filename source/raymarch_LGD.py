dfb4b6e601bc796d2582a48352526ca1a484060f
polariche <polariche@gmail.com>
2021-05-31 13:28:35 +0000

    debugging intensifies

--- source/raymarch_LGD.py
+++ source/raymarch_LGD.py
@@ -88,7 +88,7 @@ def main():
 
     p_distribution = GridDataset(mm, mx, wh)
 
-    d = torch.ones((width*height, 1), device=device, dtype=torch.float)
+    d = torch.ones((width*height, 1), device=device, dtype=torch.float).require_grad_()
     
 
     sampler = nn.Sequential(UniformSample(width*height), 
