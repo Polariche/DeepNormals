b9b7598a319dbd07167eafc0c0f39dbff555a778
polariche <polariche@gmail.com>
2021-06-05 14:49:15 +0000

    add Adam test for pc

--- source/experiments/train_LGD_raymarch.py
+++ source/experiments/train_LGD_raymarch.py
@@ -37,9 +37,9 @@ def main():
 
     parser.add_argument('--batchsize', dest='batchsize', type=int, metavar='BATCHSIZE', default=1,
                             help='batch size')
-    parser.add_argument('--epoch', dest='epoch', type=int,metavar='EPOCH', default=200, 
+    parser.add_argument('--epoch', dest='epoch', type=int,metavar='EPOCH', default=500, 
                             help='epochs for adam and lgd')
-    parser.add_argument('--lr', dest='lr', type=float,metavar='LEARNING_RATE', default=1e-3, 
+    parser.add_argument('--lr', dest='lr', type=float,metavar='LEARNING_RATE', default=5e-3, 
                             help='learning rate')
     parser.add_argument('--lgd-step', dest='lgd_step_per_epoch', type=int,metavar='LGD_STEP_PER_EPOCH', default=5, 
                             help='number of simulation steps of LGD per epoch')
