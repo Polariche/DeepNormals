9696427a69e63779e1267efafcb6ab0ab894843b
polariche <polariche@gmail.com>
2021-06-05 14:35:47 +0000

    add Adam test for pc

--- source/experiments/test_Adam_pc.py
+++ source/experiments/test_Adam_pc.py
@@ -13,13 +13,12 @@ sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) )
 from models.models import Siren
 from loaders import ObjDataset, ObjUniformSample, Dataset, UniformSample, GridDataset, PointTransform
 from models.LGD import LGD, detach_var
-from evaluate_functions import chamfer_distance, nearest_from_to, dist_from_to
+from evaluate_functions import chamfer_distance
 
 from torch.utils.data import  DataLoader, WeightedRandomSampler
 
 import argparse
 
-from sklearn.neighbors import KDTree
 
 
 def main():
@@ -31,9 +30,6 @@ def main():
     parser.add_argument('--tb-save-path', dest='tb_save_path', metavar='PATH', default='../checkpoints/', 
                             help='tensorboard checkpoints path')
 
-    parser.add_argument('--weight-save-path', dest='weight_save_path', metavar='PATH', default='../weights/', 
-                            help='weight checkpoints path')
-
     parser.add_argument('--sdf-weight', dest='sdf_weight', metavar='PATH', default=None, 
                             help='pretrained weight for SDF model')
 
@@ -42,10 +38,6 @@ def main():
                             help='batch size')
     parser.add_argument('--epoch', dest='epoch', type=int,metavar='EPOCH', default=500, 
                             help='epochs for adam and lgd')
-    parser.add_argument('--lr', dest='lr', type=float,metavar='LEARNING_RATE', default=1e-3, 
-                            help='learning rate')
-    parser.add_argument('--lgd-step', dest='lgd_step_per_epoch', type=int,metavar='LGD_STEP_PER_EPOCH', default=5, 
-                            help='number of simulation steps of LGD per epoch')
     parser.add_argument('--n', dest='n', type=int,metavar='N', default=30000, 
                             help='number of points to sample')
 
@@ -55,9 +47,7 @@ def main():
     args = parser.parse_args()
 
     n = args.n
-    lr = args.lr
     epoch = args.epoch
-    lgd_step_per_epoch = args.lgd_step_per_epoch
 
     writer = SummaryWriter(args.tb_save_path)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -97,31 +87,23 @@ def main():
     origin_eval_list = lambda x: origin_eval(x[0])
     sdf_eval_list = lambda x: sdf_eval(x[0])
 
-    print("lgd")
-    hidden = None
+    print("adam")
+    optimizer = optim.Adam([x], lr = lr)
 
-    lgd = LGD(3, 2, k=10).to(device)
-    lgd_optimizer = optim.Adam(lgd.parameters(), lr= lr)
-
-    # train LGD
-    lgd.train()
     for i in range(epoch):
-        print(i)
-        # evaluate losses
-        samples_n = n//32
-        sample_inds = torch.randperm(n)[:samples_n]
+        optimizer.zero_grad()
 
-        origin_eval_batch = lambda x: torch.pow(x_original[sample_inds] - x, 2).sum(dim=1).mean()
-        origin_eval_batch_list = lambda x: origin_eval_batch(x[0])
+        loss = sdf_eval(x)
+        loss.backward(retain_graph=True)
 
-        # update lgd parameters
-        lgd_optimizer.zero_grad()
-        lgd.loss_trajectory_backward(x[sample_inds], [origin_eval_batch_list, sdf_eval_list], None, 
-                                     constraints=["None", "Zero"], batch_size=samples_n, steps=lgd_step_per_epoch)
-        lgd_optimizer.step()
+        optimizer.step()
 
-        torch.save(lgd.state_dict(), args.weight_save_path+'model_%03d.pth' % i)
-        
+        if i%10 == 0:
+            writer.add_scalars("regression_loss", {"Adam": loss}, global_step=i)
+            writer.add_mesh("point cloud regression_Adam", x.unsqueeze(0), global_step=i)
+
+            writer.add_scalars("chamfer_distance", {"Adam": chamfer_distance(x, p)}, global_step=i)
+            
     writer.close()
 
 if __name__ == "__main__":
