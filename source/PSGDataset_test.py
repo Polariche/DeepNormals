ce73d87a6aec399b7c530717968d539fefc6261c
polariche <polariche@gmail.com>
2021-06-07 20:28:41 +0000

    add a simple test for PSG loader

---
+++ source/PSGDataset_test.py
@@ -0,0 +1,28 @@
+import numpy as np
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) ) )
+
+from models.models import Siren
+from loaders import PSGDataset
+from models.LGD import LGD, detach_var
+
+import argparse
+
+parser = argparse.ArgumentParser(description='Test',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument('data', metavar='DATA', help='path to file')
+args = parser.parse_args()
+
+psg = PSGDataset(args.data)
+
+print(psg[0])
+print(psg[0]['pc_gt'])
+print(psg[0]['pc_pred'])
+
+
+
+
