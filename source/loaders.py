c159b8bb53bdff742372d4e1a78874e75311ba13
polariche <polariche@gmail.com>
2021-06-10 08:41:31 +0000

    lgd chamfer debugging

--- source/loaders.py
+++ source/loaders.py
@@ -148,6 +148,9 @@ class PSGDataset(Dataset):
                     break
 
                 self.n = self.n + 1
+        
+        print(pc_gt[0].shape)
+        print(pc_pred[0].shape)
 
     def __len__(self):
         return self.n
