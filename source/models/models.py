a6fb40ffe792857c5db92be76f9ffcfafdbcf5f4
polariche <polariche@gmail.com>
2021-07-01 06:34:08 +0000

    add sine init funcs

--- source/models/models.py
+++ source/models/models.py
@@ -392,3 +392,13 @@ def sine_init(m):
             num_input = m.weight.size(-1)
             # See supplement Sec. 1.5 for discussion of factor 30
             m.weight.uniform_(-np.sqrt(6 / num_input) / 30, np.sqrt(6 / num_input) / 30)
+
+
+
+def first_layer_sine_init(m):
+    with torch.no_grad():
+        if hasattr(m, 'weight'):
+            num_input = m.weight.size(-1)
+            # See paper sec. 3.2, final paragraph, and supplement Sec. 1.5 for discussion of factor 30
+            m.weight.uniform_(-1 / num_input, 1 / num_input)
+
