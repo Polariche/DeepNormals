3d331f150804ffda7d06ddcf71ece98c1f213ea1
polariche <polariche@gmail.com>
2021-07-01 10:24:49 +0000

    add old source

--- source/experiments/train_LGD_pc_old.py
+++ source/experiments/train_LGD_pc_old.py
@@ -11,7 +11,7 @@ import os
 sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) ) )
 
 from models.models import Siren
-from loaders import ObjDataset, ObjUniformSample, Dataset, UniformSample, GridDataset, PointTransform, get_obj_dataloader
+from loaders import ObjDataset, Dataset, UniformSample, GridDataset, PointTransform, get_obj_dataloader
 from models.LGD import LGD, detach_var
 from evaluate_functions import chamfer_distance, nearest_from_to, dist_from_to
 
