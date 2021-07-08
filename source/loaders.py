784ad2eaac2ef16702f6bcafea208fa07f29460f
polariche <polariche@gmail.com>
2021-07-08 08:36:16 +0000

    alternative sampling method

--- source/loaders.py
+++ source/loaders.py
@@ -282,7 +282,7 @@ class SceneDataset(Dataset):
 
             dl = DataLoader(ds, 
                             batch_size=self.ray_batch_size,
-                            sampler=FixedSampler(indices),
+                            sampler=SequentialSampler,
                             shuffle=False,
                             batch_sampler=None)
             
