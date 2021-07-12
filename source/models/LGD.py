c4a73be2abf9d17734e95928a500697aa032cb79
polariche <polariche@gmail.com>
2021-07-12 06:50:05 +0000

    use BCE loss for sdf gt loss:

--- source/models/LGD.py
+++ source/models/LGD.py
@@ -283,7 +283,7 @@ class Renderer(nn.Module):
                     target.backward(grad, retain_graph=True)#, inputs=layers_params)
 
         # backpropagation for sdf (=self.sdf)
-        sdf_gt = rays['visible']
+        sdf_gt = rays['visible'].view(-1)
         sdf_gt_loss = -torch.log(sdf_gt.view(-1,1)[sdf_gt]).sum()
         sdf_gt_loss += -torch.log(1 - sdf_gt.view(-1,1)[~sdf_gt]).sum()
         sdf_gt_loss /= sdf_gt.view(-1,1).shape[0] #torch.pow((sdf_res - sdf_gt), 2).mean()
