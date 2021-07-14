e6263cabbf81d0c6f9c7bd8f54531a93f7c5a3f5
polariche <polariche@gmail.com>
2021-07-14 12:13:06 +0000

    add adam test

--- source/experiments/adam_reprojection.py
+++ source/experiments/adam_reprojection.py
@@ -38,7 +38,7 @@ def find_nearest_correspondences_dist(x_hat, x, k=1):
         x_ = x_.transpose(0,1).reshape(shp[-2], -1)
 
         knn_f = knn.apply
-        dist_, _ = knn_f(x_hat_,x_,k)
+        dist_ = knn_f(x_hat_,x_,k)
         dists.append(dist_.mean().unsqueeze(0) / shp[-3])
 
     dist = torch.cat(dists).sum()
