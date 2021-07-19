3c68c191b9fea11010723c9e514d08787bc64296
polariche <polariche@gmail.com>
2021-07-19 11:20:52 +0000

    add clgd & clgd experiment

---
+++ source/experiments/CLGD_reprojection.py
@@ -0,0 +1,106 @@
+import numpy as np
+import torch 
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+from torch.utils.tensorboard import SummaryWriter
+
+import sys
+import os
+sys.path.append( os.path.dirname( os.path.dirname( os.path.abspath(__file__) ) ) )
+
+#from models.models import SingleBVPNet, DeepSDFDecoder, Siren
+from loaders import CategoryDataset, dict_collate_fn, dict_to_device
+from models.LGD import Projector
+from torch.utils.data import  DataLoader
+
+import argparse
+
+
+import time
+from tqdm.autonotebook import tqdm
+from knn import knn
+import utils
+
+
+def main():
+    parser = argparse.ArgumentParser(description='Test',
+                                 formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--tb-save-path', dest='tb_save_path', type=str, metavar='PATH', default='../checkpoints/', 
+                            help='tensorboard checkpoints path')
+
+    parser.add_argument('--weight-save-path', dest='weight_save_path', type=str, metavar='PATH', default='../weights/', 
+                            help='weight checkpoints path')
+
+
+    parser.add_argument('--batchsize', dest='batchsize', type=int, metavar='BATCHSIZE', default=1,
+                            help='batch size')
+    parser.add_argument('--epoch', dest='epoch', type=int,metavar='EPOCH', default=500, 
+                            help='epochs for adam and lgd')
+    parser.add_argument('--lr', dest='lr', type=float,metavar='LEARNING_RATE', default=1e-3, 
+                            help='learning rate')
+    parser.add_argument('--lgd-step', dest='lgd_step_per_epoch', type=int,metavar='LGD_STEP_PER_EPOCH', default=5, 
+                            help='number of simulation steps of LGD per epoch')
+
+
+    parser.add_argument('--outfile', dest='outfile', metavar='OUTFILE', 
+                            help='output file')
+
+    args = parser.parse_args()
+
+    writer = SummaryWriter(args.tb_save_path)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+
+    category = CategoryDataset(srn_dir="/data/SRN/cars_train", 
+                                shapenet_dir="/data/shapenetv2/ShapeNetCore.v2", 
+                                img_sidelength=512, 
+                                batch_size=args.batchsize, 
+                                ray_batch_size=3)
+
+    category_loader = DataLoader(category, batch_size=1, shuffle=True)
+
+
+    clgd = CLGD.to(device)
+    clgd_optimizer = optim.Adam(clgd.parameters(), lr=args.lr)
+
+    # train LGD
+    clgd.train()
+    with tqdm(total=args.epoch) as pbar:
+        for i in range(args.epoch):
+            start_time = time.time()
+
+            samples = next(iter(category_loader))
+            samples = dict_to_device(samples, device)
+
+            X = (torch.randn_like(samples['p']) * 2e-1).requires_grad_(True)
+            H = (torch.randn(*samples['p'].shape[:-2], 1, 256, device=device).requires_grad_(True)
+            G = (torch.randn(*samples['p'].shape[:-3], 256, device=device).requires_grad_(True)
+            P = samples['pose']
+
+            Y = samples['p']
+
+            clgd.Y = Y
+
+            clgd_optimizer.zero_grad()
+            X_new, X_new_grad = clgd_optimizer.backward(X, H, P, G)
+            clgd_optimizer.step()
+
+            writer.add_mesh("input_view",
+                            (samples['p']).reshape(-1,3).unsqueeze(0),
+                            global_step=i+1,
+                            colors=(torch.clamp((F.normalize(samples['n'], dim=-1).reshape(-1,3).unsqueeze(0)), -1, 1) * 128 + 128).int())
+
+            writer.add_mesh("output_view",
+                            (X_new).reshape(-1,3).unsqueeze(0),
+                            global_step=i+1,
+                            colors=(F.normalize(X_new_grad, dim=-1).reshape(-1,3).unsqueeze(0) * 128 + 128).int())
+
+            pbar.update(1)
+
+    writer.close()
+
+if __name__ == "__main__":
+    main()
--- source/models/CLGD.py
+++ source/models/CLGD.py
@@ -11,24 +11,57 @@ import knn_cuda
 from models import DeepSDFDecoder
 
 import numpy as np
+from knn import knn
+import utils
 
 class CLGD(nn.Module):
     """
-    h : (b, 1, 1, ?)
     X : (b, 1, n, 3)
-    P : (b, m, 1, 5)
-    u : (b, m, n, 2)
+    H : (b, 1, 1, 256)
+    P : (b, m, 1, 6)
+    G : (b, 1, n, 256)
     """
 
     def __init__(self):
-        self._sdf = DeepSDFDecoder(latent_size=256, dims=[512]*8, latent_in=(4))
+        self._sdf = DeepSDFDecoder(latent_size=256, dims=[512]*8, latent_in=(4), use_tanh=True)
 
-        pass
+        self._r_layers = nn.Sequential(nn.Linear(256*3 + 13, 256, bias=False), nn.ReLU())
+        self._z_layers = nn.Sequential(nn.Linear(256*3 + 13, 256, bias=False), nn.ReLU())
+
+        self._g_layers = nn.Sequential(nn.Linear(256*3 + 13, 256, bias=False), nn.ReLU())
+
+        self._dx_layers = nn.Sequential(nn.Linear(256*3 + 13, 1, bias=False), nn.ReLU())
+        self._dh_layers = nn.Sequential(nn.Linear(256*3 + 13, 1, bias=False), nn.ReLU())
+        self._lc_layers = nn.Sequential(nn.Linear(256*3 + 13, 1, bias=False), nn.ReLU())
+        self._lp_layers = nn.Sequential(nn.Linear(256*3 + 13, 1, bias=False), nn.ReLU())
+
+
+    def expand(self, X, dim, target):
+        targets = {-1, -1, -1}
+
+        for d, t in zip(dim, target):
+            targets[d+3] = t
+            
+        return X.expand(*([-1]*(len(X.shape)-3)), *[targets[i] for i in range(3)])
+
+    def expand_all(self, X, H, P, s, dsx, dsh, G):
+        X_expand = self.expand(X, (-3), (m))
+        H_expand = self.expand(H, (-3, -2), (m, n))
+        P_expand = self.expand(P, (-2), (n))
+        
+        s_expand = self.expand(s, (-3), (m))
+        dsx_expand = self.expand(dsx, (-3), (m))
+        dsh_expand = self.expand(dsx, (-3, -2), (m, n))
+
+        G_expand = self.expand(G, (-3), (m))
+
+        return [X_expand, H_expand, P_expand, s_expand, dsx_expand, dsh_expand, G_expand]
+
 
     def sdf(self, X, H, return_grad=True):
-        H_expand = H.expand([-1]*(len(H.shape)-3), -1, X.shape[-2], -1)
+        H_expand = self.expand(H, (-2), (X.shape[-2]))
 
-        # deepsdf decoder requires (h, x) input
+        # deepsdf decoder requires [h, x] input
         _input = torch.cat([H_expand, X], dim=-1)
         _output = self._sdf(_input)
 
@@ -50,17 +83,158 @@ class CLGD(nn.Module):
 
             return _output
 
-    def forward(self, X, P, H):
+    def gru(self, X, H, P, s, dsx, dsh, G):
+        m = P.shape[-3]
+        n = X.shape[-2]
+
+        # expand every input & concat
+        expands = self.expand_all(X,H,P,s,dsx,dsh,G)
+
+        _input = torch.cat(expands, dim=-1)
+
+        r = self._r_layers(_input)                          # coefficient of old G into new G network
+        z = self._z_layers(_input)                          # interpolation variable (old G : new G)
+
+        _input = torch.cat([_input[..., :-G.shape[-1]], 
+                            r * G_expand], dim=-1)
+
+        new_G = self._g_layers(_input)
+        new_G = torch.max(new_G, dim=-3, keepdim=True)[0]      # pooling w.r.t cam dimension
+
+        G = z * new_G + (1-z) * G
+        G_expand = self.expand(G, (-3), (m))
+
+        return G
+
+        
+    def forward(self, X, H, P, G):
         
         s, dsx, dsh = self.sdf(X, H, return_grad=True)
+        G = self.gru(X, H, P, s, dsx, dsh, G)
 
+        expands = self.expand_all(X,H,P,s,dsx,dsh,G)
+
+        _input = torch.cat(expands, dim=-1)
+
+        dx = self._dx_layers(_input)
+        dh = self._dh_layers(_input)
+        lc = self._lc_layers(_input)
+        lp = self._lp_layers(_input)
+
+        dx = torch.max(dx, dim=-3, keepdim=True)[0]
+        dh = torch.max(dx, dim=(-3, -2), keepdim=True)[0]
+        lc = torch.max(lc, dim=-2, keepdim=True)[0]
 
         # s, dsx, dsh : output from SDF
-        # dx, dh,  lam, dp : output from 
-        return s, dsx, dsh,  dx, dh,  lam, dp 
+        # dx, dh,  lc, lp : output from 
+        return [s, dsx, dsh, G,  dx, dh,  lc, lp] 
 
-    def step(self, X, P, H):
-        s, dsx, dsh,  dx, dh,  lam, dp = self(X, P, H)
+    def step(self, X, H, P, G):
+        forward_inputs = self(X, H, P, G)
+        [s, dsx, dsh,  G,  dx, dh,  lc, lp] = forward_inputs
 
-        X + dx * dsx
-        H + dh * dsh
+        X = X + dx * dsx
+        H = H + dh * dsh
+
+        return X, H, G, forward_inputs
+
+    def backward(self, X, H, P, G, steps=5):
+        for i in range(steps):
+            X, H, G, forward_inputs = self.step(X,H,P,G)
+
+        [s, dsx, dsh,  G,  dx, dh,  lc, lp] = forward_inputs
+        s, dsx_, dsh_ = self.sdf(X, H, return_grad=True)
+
+
+        L1, L2, L3, L4 = self.total_loss(X,H, P, s, dsx, lc, lp)
+        L = L1 + L2 + L3 + L4
+
+        L_grad_targets = [s, dsx, dsh, dx, dh, lc, lp]
+
+        L_grads = torch.autograd.grad(L, 
+                                      L_grad_targets, 
+                                      grad_outputs=torch.ones_like(L), 
+                                      create_graph=False,
+                                      retain_graph=True)
+
+        for i, (t, g) in enumerate(zip(L_grad_targets, L_grads)):
+            if g is not None:
+                if i >= 5:
+                    t.backward(- g, retain_graph=True)
+                else:
+                    t.backward(g, retain_graph=True)
+
+        return X, dsx_
+
+        
+
+
+    def total_loss(self, X,H,P,s,dsx, lc, lp):
+        L1 = self.projection_loss(X, P, lc)     # weighted reprojection loss
+
+        L2 = self.grad_loss(dsx, lp)            # |dsx| = 1 loss
+
+        L3 = self.sdf_loss(X, X, P, s)          # BCE(s, repr(X,P))
+
+        L4 = self.H_loss(H)                     # regularization on |H|
+
+        return L1, L2, L3, L4
+
+    
+    def projection_loss(self, X, P, lc):
+        Y = self.Y
+
+        lc = torch.sqrt(lc)
+
+        x = utils.project(X, P) * lc
+        y = utils.project(Y, P) * lc
+
+        return find_nearest_correspondences_dist(x,y)
+
+    
+    def grad_loss(self, dsx, lp):
+        return torch.pow(torch.sqrt(torch.pow(dsx, 2).sum(dim=-1, keepdim=True)) - 1, 2) * lp
+
+    def sdf_loss(self, X, P, s):
+        shp = s.shape
+        bce = nn.BCELoss(reduction=False)
+
+        Y = self.Y
+        x = self.project(X, P)
+        y = self.project(Y, P)
+
+        true_s2 = find_nearest_correspondences_dist(x,y) / P.shape[-3]
+        true_s2 = torch.clamp(true_s2, 0, 1)
+
+        return bce(torch.pow(s,2), true_s2)
+
+    def H_loss(self, H):
+        return torch.pow(H, 2).sum(dim=-1, keepdim=True)
+
+
+
+    def find_nearest_correspondences_dist(self, x_hat, x, k=1):
+        assert x_hat.shape[-1] == x.shape[-1]
+        assert x_hat.shape[-3] == x.shape[-3]
+        shp1 = x_hat.shape
+        shp2 = x.shape
+
+        # ((batches), m, n, 2)
+        x_hat = x_hat.view(-1, shp1[-3], shp1[-2], shp1[-1])
+        x = x.view(-1, shp2[-3], shp2[-2], shp2[-1])
+
+        dists = []
+        for x_hat_, x_ in zip(x_hat, x):
+            # ((batches), n, m*2)
+            x_hat_ = x_hat_.transpose(0,1).reshape(shp1[-2], -1)    
+            x_ = x_.transpose(0,1).reshape(shp2[-2], -1)
+
+            knn_f = knn.apply
+            dist_  = knn_f(x_hat_,x_,k)
+            dists.append(dist_.mean(dim=1).unsqueeze(0))
+
+        # ((batches), n)
+        dist = torch.cat(dists).view(shp[:-3], x_hat.shape[-2]).unsqueeze(-1).unsqueeze(-3)
+
+        return dist
+    
